c8b169000d555de0bbde65ea32b339eb2ef4f8e0
ChadwickBlewett <ChadwickBlewett@gmail.com>
2020-12-05 07:09:28 -0800

    Added active tabs to nav

--- restaurant-page/src/components/navComponent.js
+++ restaurant-page/src/components/navComponent.js
@@ -3,11 +3,22 @@ const navComponent = () => {
     component.classList.add('nav');
     component.innerHTML = `
         <ul class="nav-bar center">
-            <li class="nav-item"><a href="#" data-url="/" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" data-url="/" class="nav-link nav-link-active">Home</a></li>
             <li class="nav-item"><a href="#" data-url="/menu" class="nav-link">Menu</a></li>
             <li class="nav-item"><a href="#" data-url="/contact" class="nav-link">Contact</a></li>
         </ul>
     `
+
+    component.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.target.tagName === 'A') {
+            const links = component.querySelectorAll('a');
+            for (const link of links) {
+                link.classList.remove('nav-link-active');
+            }
+            e.target.classList.add('nav-link-active');
+        }
+    }, false);
 
     return component
 }
