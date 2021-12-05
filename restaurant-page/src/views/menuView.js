f4346a094e0e89133634f7e3e0141d0db3f399c7
ChadwickBlewett <ChadwickBlewett@gmail.com>
2021-12-05 05:43:57 -0800

    Updated restaurant-page, fixed links

--- restaurant-page/src/views/menuView.js
+++ restaurant-page/src/views/menuView.js
@@ -1,25 +1,25 @@
 import MenuItem from '../factories/MenuItemFactory.js';
 
 const menuView = () => {
-    const view = document.createElement('div');
-    view.classList.add('content', 'menu-content');
+  const view = document.createElement('div');
+  view.classList.add('content', 'menu-content');
 
-    const menuItems = [
-        MenuItem('Burger', 'the classic', '29.99', 'burger.jpg'),
-        MenuItem('Taco', 'do not look at the price please just buy it', '349.95', 'taco.jpg'),
-        MenuItem('Bottled Water', 'nothing in life is free buddy', '3.99', 'bottled-water.jpg'),
-    ];
+  const menuItems = [
+    MenuItem('Burger', 'the classic', '29.99', 'burger.jpg'),
+    MenuItem('Taco', 'no refunds', '349.95', 'taco.jpg'),
+    MenuItem('Bottled Water', 'high quality h2o', '3.99', 'bottled-water.jpg'),
+  ];
 
-    for (const item of menuItems) {
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('menu-item', 'text-area');
+  for (const item of menuItems) {
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('menu-item', 'text-area');
 
-        const name = item.getName();
-        const imageSRC = item.getImageSRC();
-        const description = item.getDescription();
-        const price = item.getPrice();
+    const name = item.getName();
+    const imageSRC = item.getImageSRC();
+    const description = item.getDescription();
+    const price = item.getPrice();
 
-        itemDiv.innerHTML = `
+    itemDiv.innerHTML = `
             <img src="${imageSRC}" alt="${name}" class="menu-item-img">
             <div class="menu-item-text">
                 <h4 class="big">
@@ -29,10 +29,10 @@ const menuView = () => {
                 <span class="big">${description}</span>
             </div>
         `;
-        view.appendChild(itemDiv);
-    }
+    view.appendChild(itemDiv);
+  }
 
-    return view;
-}
+  return view;
+};
 
 export default menuView;
